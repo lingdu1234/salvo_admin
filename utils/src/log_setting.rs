2bff09494feaf0ae38bfeff86dc677f1f8666786
lingdu <waong2005@126.com>
2024-03-23 19:03:53 +0800

    update deps

--- utils/src/log_setting.rs
+++ utils/src/log_setting.rs
@@ -4,26 +4,17 @@ use tracing_subscriber::fmt::{
     self,
     format::{Compact, Format},
 };
+#[cfg(target_os = "windows")]
+use time::format_description::well_known::Rfc3339;
+#[cfg(target_os = "windows")]
+use tracing_subscriber::fmt::time::LocalTime;
+#[cfg(target_os = "windows")]
 
 // 设置日志格式
 pub fn set_log_format() -> Format<Compact, LocalTime<Rfc3339>> {
     fmt::format()
         .with_level(true)
         .with_target(true)
-        .with_thread_ids(true)
-        .with_timer(LocalTime::rfc_3339()) // use RFC 3339 timestamps
-        .compact()
-}
-
-#[cfg(target_os = "windows")]
-use time::format_description::well_known::Rfc3339;
-#[cfg(target_os = "windows")]
-use tracing_subscriber::fmt::time::LocalTime;
-#[cfg(target_os = "windows")]
-pub fn get_log_format() -> Format<Compact, LocalTime<Rfc3339>> {
-    fmt::format()
-        .with_level(true) // don't include levels in formatted output
-        .with_target(true) // don't include targets
         .with_thread_ids(true)
         .with_timer(LocalTime::rfc_3339()) // use RFC 3339 timestamps
         .compact()
